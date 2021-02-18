1381bad3d67bf4f249c62366bf0aace3790be19c
DevonCallanan <devoncallanan@gmail.com>
2021-02-17 20:01:09 -0500

    HW 2 done

--- HW2/lr.py
+++ HW2/lr.py
@@ -2,22 +2,26 @@
 # University of Pittsburgh
 # ECE 2371 Pattern Rec
 
+import random
 import numpy
 from scipy import io
 import matplotlib.pyplot as plt
 
 
 def g(t):
+    # print(t)
     val = 1/(1+numpy.exp(-t))
+    # print(val)
     return val
 
-def sse(dataset, theta):
+def log_loss(dataset, theta):
     x_hats = [numpy.array([1,x[0],x[1]]).reshape((-1,1)) for x in points]
-    sse = 0
+    log_loss = 0
     for x_hat, label in zip(x_hats,labels):
         t = numpy.matmul(theta.T, x_hat)
-        sse += label*numpy.log(g(t)) + (1-label)*numpy.log(1-g(t))
-    return sse
+        log_loss += label*numpy.log(g(t)) + (1-label)*numpy.log(1-g(t))
+    # print(log_loss)
+    return numpy.abs(log_loss[0][0])/len(dataset)
 
 
 def gradient_descent(dataset):
@@ -26,12 +30,16 @@ def gradient_descent(dataset):
     # initialize decision rule guess
     theta = numpy.random.rand(dims + 1, 1)
     x_hats = [numpy.array([1,x[0],x[1]]).reshape((-1,1)) for x in points]
-    alpha = .1
+    alpha = .01
     gradient = numpy.ones((dims + 1, 1))
 
     iterations = 0
+    plot_points = []
+    last_log = log_loss(dataset, theta)
+    delta = 1
 
-    while numpy.abs(gradient.sum()) > .1 and iterations < 10000:
+    while delta > .1 and iterations < 10000:
+        # print(log_loss(dataset, theta))
         iterations += 1
         gradient = numpy.zeros((dims + 1, 1))
         for x_hat, label in zip(x_hats,labels):
@@ -39,14 +47,17 @@ def gradient_descent(dataset):
             t = numpy.matmul(theta.T, x_hat)
             gradient = gradient + x_hat*(label-g(t))
 
+        # plot_points.append(log_loss(dataset, theta))
         theta = theta + gradient*alpha
-        # print(sse(dataset, theta))
-    # print(theta)
+        next_log = log_loss(dataset, theta)
+        delta = numpy.abs(last_log - next_log)
+        last_log = next_log
+        # print(gradient)
+        # print(delta)
+
     print("After " + str(iterations) + " iterations gradient_descent is complete")
-    # theta = theta/(numpy.sqrt(theta[1]**2+ theta[2]**2))
-    theta = theta/numpy.linalg.norm(theta)
-    # print(theta)
-    return theta
+
+    return theta, plot_points
 
 def newton(dataset):
     points, labels = dataset
@@ -57,8 +68,11 @@ def newton(dataset):
     gradient = numpy.ones((dims + 1, 1))
 
     iterations = 0
+    plot_points = []
+    last_log = log_loss(dataset, theta)
+    delta = 1
 
-    while numpy.abs(gradient.sum()) > .01 and iterations < 10000:
+    while delta > .1 and iterations < 1000:
         iterations += 1
         hessian = numpy.zeros((dims + 1, dims + 1))
         gradient = numpy.zeros((dims + 1, 1))
@@ -68,15 +82,20 @@ def newton(dataset):
             gradient += x_hat*(label-g(t))
             hessian += x_hat.dot(x_hat.T)*g(t)*(1-g(t))
             # print(hessian)
-        # print(sse(dataset, theta))
 
+        # theta_pre = theta
+        # plot_points.append(log_loss(dataset, theta_pre))
         theta = theta + (numpy.linalg.inv(hessian)).dot(gradient)
+        next_log = log_loss(dataset, theta)
+        delta = numpy.abs(last_log - next_log)
+        last_log = next_log
     # print(theta)
     print("After " + str(iterations) + " iterations gradient_descent is complete")
     # theta = theta/(numpy.sqrt(theta[1]**2+ theta[2]**2))
-    theta = theta/numpy.linalg.norm(theta)
+    # theta = theta/numpy.linalg.norm(theta)
     # print(theta)
-    return theta
+    # print(iterations)
+    return theta, plot_points
 
 def stochastic(dataset):
     points, labels = dataset
@@ -84,30 +103,42 @@ def stochastic(dataset):
     # initialize decision rule guess
     theta = numpy.random.rand(dims + 1, 1)
     x_hats = [numpy.array([1,x[0],x[1]]).reshape((-1,1)) for x in points]
-    alpha = .01
+    alpha = .1
     gradient = numpy.ones((dims + 1, 1))
 
+    plot_points = []
+
     iterations = 0
+    grouped = [(x_hat, label) for x_hat, label in zip(x_hats, labels)]
+    numpy.random.shuffle(grouped)
 
-    while numpy.abs(gradient.sum()) > .01 and iterations < 10000:
+    delta = 1
+
+    last_log = log_loss(dataset, theta)
+
+    while delta > .001:
+        # print(log_loss(dataset, theta))
         # gradient = numpy.zeros((dims + 1, 1))
-        for x_hat, label in zip(x_hats,labels):
-            iterations += 1
-            # sum gradient accross all inputs
-            t = numpy.matmul(theta.T, x_hat)
-            gradient = x_hat*(label-g(t))
-            # print(sse(dataset, theta))
+        x_hat, label = random.choice(grouped)
+        iterations += 1
+        # sum gradient accross all inputs
+        t = numpy.matmul(theta.T, x_hat)
+        gradient = x_hat*(label-g(t))
+        # plot_points.append(numpy.linalg.norm(gradient))
+        plot_points.append(log_loss(dataset, theta))
+        # print(numpy.linalg.norm(gradient))
 
-            theta = theta + gradient*alpha
-            if numpy.abs(gradient.sum()) <= .01 or iterations > 10000:
-                break
+        theta = theta + gradient*alpha
+        next_log = log_loss(dataset, theta)
+        delta = numpy.abs(last_log - next_log)
+        last_log = next_log
 
-    # print(theta)
     print("After " + str(iterations) + " iterations gradient_descent is complete")
     # theta = theta/(numpy.sqrt(theta[1]**2+ theta[2]**2))
-    theta = theta/numpy.linalg.norm(theta)
+    # theta = theta/numpy.linalg.norm(theta)
     # print(theta)
-    return theta
+    # print(iterations)
+    return theta, plot_points
 
 
 def test(dataset, rule):
@@ -130,7 +161,7 @@ def test(dataset, rule):
 
 
 # can't seem to get this to work
-def plot(dataset, rule):
+def plot_rule(dataset, rule):
     points, labels = dataset
     # for plotting the data to file. allows visualization of lda and troubleshooting
     one_points = numpy.array([point for point, label in zip(points, labels) if label == 1])
@@ -139,29 +170,42 @@ def plot(dataset, rule):
     plt.scatter(zero_points.T[0], zero_points.T[1], color='red')
     axes = plt.gca()
     axes.set_aspect('equal')
+    axes.set_ylim(-2, 3)
     x_vals = numpy.array(axes.get_xlim()).reshape(-1,1)
     # y_vals = rule[2]/rule[1] * x_vals# - rule[0]
     angle = numpy.arctan2(rule[2],rule[1])
     b_pointx = -rule[0]*numpy.cos(angle)
     b_pointy = -rule[0]*numpy.sin(angle)
-    y_vals = -rule[2]/rule[1] *( x_vals - b_pointx) + b_pointy
-    plt.scatter(b_pointx, b_pointy, color='black')
+    y_vals = - (rule[1]*x_vals + rule[0])/rule[2] #+ b_pointy
+    # plt.scatter(b_pointx, b_pointy, color='black')
     plt.plot(x_vals, y_vals, '--')
-    plt.ylabel("points")
+    # plt.ylabel("points")
     plt.show()
+
+def plot_descent(log, stoch, iterations):
+    axes = plt.gca()
+    plt.plot(range(iterations), log, label="Regular")
+    plt.plot(range(iterations), newt, label="Newtonian")
+    plt.plot(range(iterations), stoch, label="Stochastic")
+    axes.legend()
+    # plt.xlim(left=-5)
+    plt.xscale('log')
+    plt.show()
+
 
 if __name__ == '__main__':
     # print("Select algorithm")
 
-    matfile = io.loadmat("LRdata/synthetic1.mat")
+    matfile = io.loadmat("LRdata/synthetic4.mat")
     points = numpy.array(matfile["X"]).T
     labels = numpy.array(matfile["Y"]).T
     dataset = (points, labels)
 
-    rule = gradient_descent(dataset)
+    # rule, log = gradient_descent(dataset)
+    # test(dataset, rule)
+    # rule, newt = newton(dataset)
+    # test(dataset, rule)
+    rule, stoch = stochastic(dataset)
     test(dataset, rule)
-    rule = newton(dataset)
-    test(dataset, rule)
-    rule = stochastic(dataset)
-    test(dataset, rule)
-    # plot(dataset, rule)
+    plot_rule(dataset, rule)
+    # plot_descent(log, newt, stoch, 1000)
